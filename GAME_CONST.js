d3986edc61229b5af6ac9c1e279e7d3607edf401
digvijay96 <digvijay.iiti@gmail.com>
2018-04-13 14:29:41 +0530

    Create Make7 Game

---
+++ GAME_CONST.js
@@ -0,0 +1,23 @@
+const GAME_CONST = {
+    CANVAS : {
+        WIDTH : 1080,
+        HEIGHT: 1920,
+        CONTAINER: "uss-callister"
+    },
+    STATES: {
+        BOOT: "Boot",
+        PRELOAD: "Preload",
+        PLAY: "Play"
+    },
+    MUSIC: {
+        VOLUME: {
+            THEME: 0.2,
+            TAP: 0.1
+        }
+    },
+    SPEED: {
+        METEOR : 400,
+        SHOOT : 600,
+        SPACE_SHIP : 0.08
+    }
+};
---
+++ HexCell.js
@@ -0,0 +1,4 @@
+function createCellObject(key, frame, spriteObject) {
+    var cell = {key: key, frame: frame, sprite: spriteObject};
+    return cell;
+}
